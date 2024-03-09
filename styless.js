18e8ba723d7464983d5bb5ab820a25bc9cab912a
Jeremiah <ayamjym12@gmail.com>
2024-03-09 15:46:39 +0100

    js commit

--- styless.js
+++ styless.js
@@ -1,3 +1,5 @@
 var visitorName = prompt('what is your name');
 console.log(visitorName);
 // we want to write a comment 
+var isConfirm = comnfirm('are you sure you want to confirm');
+console.log(isConfirm);
